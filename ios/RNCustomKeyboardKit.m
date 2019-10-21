9133a8180647bbf7ebedd464255914d63c266c98
valeri <valeritsert@gmail.com>
2019-10-21 23:20:52 +0400

    use backedTextInputView, backspace

--- ios/RNCustomKeyboardKit.m
+++ ios/RNCustomKeyboardKit.m
@@ -1,4 +1,3 @@
-
 #import <React/RCTRootView.h>
 #import "RNCustomKeyboardKit.h"
 #import "RCTBridge+Private.h"
@@ -27,15 +26,17 @@ RCT_EXPORT_METHOD(install:(nonnull NSNumber *)reactTag withType:(nonnull NSStrin
 RCT_EXPORT_METHOD(uninstall:(nonnull NSNumber *)reactTag)
 {
   RCTSinglelineTextInputView *view = [_bridge.uiManager viewForReactTag:reactTag];
-  UITextField *textView = view.backedTextInputView;
+    UITextField *textView = view.backedTextInputView;
+    textView.inputView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 0, 0)];
   textView.inputView = nil;
   [textView reloadInputViews];
 }
 
 RCT_EXPORT_METHOD(insertText:(nonnull NSNumber *)reactTag withText:(NSString*)text) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
+  RCTSinglelineTextInputView *view = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+  UITextField *textView = view.backedTextInputView;
 
-  [view replaceRange:view.selectedTextRange withText:text];
+  [textView replaceRange:textView.selectedTextRange withText:text];
 }
 
 RCT_EXPORT_METHOD(setText:(nonnull NSNumber *)reactTag withText:(NSString*)text) {
@@ -59,13 +60,14 @@ RCT_EXPORT_METHOD(hideStandardKeyboard:(nonnull NSNumber *)reactTag) {
 }
 
 RCT_EXPORT_METHOD(backSpace:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
-  UITextRange* range = view.selectedTextRange;
-  if ([view comparePosition:range.start toPosition:range.end] == 0) {
-    range = [view textRangeFromPosition:[view positionFromPosition:range.start offset:-1] toPosition:range.start];
+  RCTSinglelineTextInputView *view = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+  UITextField *textView = view.backedTextInputView;
+    
+  UITextRange* range = textView.selectedTextRange;
+  if ([textView comparePosition:range.start toPosition:range.end] == 0) {
+    range = [textView textRangeFromPosition:[textView positionFromPosition:range.start offset:-1] toPosition:range.start];
   }
-  [view replaceRange:range withText:@""];
+  [textView replaceRange:range withText:@""];
 }
 
 RCT_EXPORT_METHOD(doDelete:(nonnull NSNumber *)reactTag) {
